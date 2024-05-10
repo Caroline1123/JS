f6e4b52a94976bb950df3af5da69eab00c2b3c11
Caroline <deconinck.caro@gmail.com>
2024-05-10 12:30:32 +0200

    added loader

--- 3.async/exercise3/assets/js/script.js
+++ 3.async/exercise3/assets/js/script.js
@@ -1,15 +1,22 @@
 const main = document.querySelector("main");
 const endpoint = "https://thatsthespir.it/api";
 const quoteBtn = document.querySelector("#show_quote");
+const loader = document.querySelector('#loader')
+
 
 const getQuote = async () => {
     try {
+        loader.style.display = "block";
         const result = await fetch("https://thatsthespir.it/api");
-        const data = await result.json()
+        const data = await result.json();
+        loader.style.display = 'none';
         createQuote(data);
     }
     catch(error) {
         alert("Failed to get you a quote! :-(")
+    }
+    finally {
+        loader.style.display = "none"
     }
 }
 
@@ -22,11 +29,15 @@ const createQuote = (data) => {
     </div>
     <div class="author">- ${data.author}</div>
     `
+    loader.style.display = "none";
     main.appendChild(quoteDiv);
 }
 
 getQuote();
 
 quoteBtn.addEventListener("click", () => {
-    getQuote();
+    loader.style.display = "block";
+    const quoteDiv = document.querySelector(".quote");
+    quoteDiv.innerHTML = "";
+    setTimeout(getQuote, 400);
 })
